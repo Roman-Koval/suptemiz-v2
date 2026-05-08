ed878e796918a3934b51a7efb3f07dfe258c3e21
Roman Koval <77807168+Roman-Koval@users.noreply.github.com>
2026-05-08 23:14:10 +0300

    Update Firebase configuration
    Updated Firebase configuration with new project details.

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,10 @@
 // ВАЖНО: сюда вставь свои данные из Firebase Console (Project settings → Web app)
 export const firebaseConfig = {
-  apiKey: "AIzaSyDcsvkcECnkcIvJxa6FRpUurgIUgYwW4qg",
-  authDomain: "suptemiz.firebaseapp.com",
-  databaseURL: "https://suptemiz-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "suptemiz",
-  storageBucket: "suptemiz.firebasestorage.app",
-  messagingSenderId: "399676923890",
-  appId: "1:399676923890:web:44657bbb2870a7d76cd56b"
+  apiKey: "AIzaSyAkXcNafkOEPf0BNTayD9qVBIFWlN_goXU",
+  authDomain: "suptemiz-v2.firebaseapp.com",
+  projectId: "suptemiz-v2",
+  storageBucket: "suptemiz-v2.firebasestorage.app",
+  messagingSenderId: "1092301740275",
+  appId: "1:1092301740275:web:0cde26482ae308336fbf86",
+  measurementId: "G-GXBD8ENR4P"
 };
