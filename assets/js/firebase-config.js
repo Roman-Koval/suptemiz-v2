b66bd2aa409421bde28b39f9bac2552cc32fea79
Roman Koval <77807168+Roman-Koval@users.noreply.github.com>
2026-05-08 22:17:07 +0300

    Create firebase-config.js with Firebase settings
    Added Firebase configuration with project details.

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+// ВАЖНО: сюда вставь свои данные из Firebase Console (Project settings → Web app)
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcsvkcECnkcIvJxa6FRpUurgIUgYwW4qg",
+  authDomain: "suptemiz.firebaseapp.com",
+  databaseURL: "https://suptemiz-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "suptemiz",
+  storageBucket: "suptemiz.firebasestorage.app",
+  messagingSenderId: "399676923890",
+  appId: "1:399676923890:web:44657bbb2870a7d76cd56b"
+};
